9733cc870183476060f63b2189b8157f91428d06
remontti <rudiremontti@gmail.com>
2020-05-06 11:08:17 -0300

    Update index.php
    Adicionado sequencial para chain

--- index.php
+++ index.php
@@ -14,6 +14,10 @@
 			document.formulario.nome.disabled=0;
 	}
 	</script>
+	<link rel="icon" href="https://blog.remontti.com.br/wp-content/uploads/2020/04/cropped-Tux-icon-32x32.png" sizes="32x32" />
+	<link rel="icon" href="https://blog.remontti.com.br/wp-content/uploads/2020/04/cropped-Tux-icon-192x192.png" sizes="192x192" />
+	<link rel="apple-touch-icon" href="https://blog.remontti.com.br/wp-content/uploads/2020/04/cropped-Tux-icon-180x180.png" />
+	<meta name="msapplication-TileImage" content="https://blog.remontti.com.br/wp-content/uploads/2020/04/cropped-Tux-icon-270x270.png" />
 </head>
 <body>
 	<?php 
@@ -22,45 +26,51 @@
 
 	<div style="padding: 30px;">
 		<form method="POST" name="formulario">
-			<h3>Gerador de CGNAT para RouterOS com netmap</h3>
+			<h3><b>Gerador de CGNAT para RouterOS com netmap</b></h3>
 			<div class="form-row">
 				<div class="form-group col-md-6">
-					<label for="c"><b>IP inicial inválido</b></label>
+					<label for="c"><b>IP inicial invÃ¡lido</b></label>
 					<input type="text" class="form-control" id="c" name="c" placeholder="ex: 100.64.0.0">
 				</div>
 				<div class="form-group col-md-6">
-					<label for="t"><b>1 público para quantos inválidos?</b></label>
-					<select class="custom-select" name="t" id="t">
-					<option value="2">2 (~32000 portas por IP Público)</option>
-					<option value="4">4 (~16000 portas por IP Público)</option>
-					<option value="8">8 (~8000 portas por IP Público)</option>
-					<option value="16">16 (~4000 portas por IP Público)</option>
-					<option value="32">32 (~2000 portas por IP Público)</option>
-					<option value="64" selected="">64 (~1000 portas por IP Público)</option>
-					<option value="128">128 (~500 portas por IP Público) Não recomendado</option>
-					</select>
-				</div>	
+                                        <label for="s"><b>Bloco PÃºblico</b></label>
+                                        <input type="text" class="form-control" id="s" name="s" placeholder="ex: 200.200.200.0/26">
+                                </div>
 			</div>
 			<div class="form-row">
-				<div class="form-group col-md-6">
-					<label for="s"><b>Bloco Público</b></label>
-					<input type="text" class="form-control" id="s" name="s" placeholder="ex: 200.200.200.0/26">			
-				</div>
-				<div class="form-group col-md-6">
-					<label for="intpub"><b>Nome da interface para adicionar os IPs Públicos</b></label>
+				<div class="form-group col-md-4">
+                                        <label for="t"><b>1 pÃºblico para quantos invÃ¡lidos?</b></label>
+                                        <select class="custom-select" name="t" id="t">
+                                        <option value="2">2 (~32000 portas por IP PÃºblico)</option>
+                                        <option value="4">4 (~16000 portas por IP PÃºblico)</option>
+                                        <option value="8">8 (~8000 portas por IP PÃºblico)</option>
+                                        <option value="16">16 (~4000 portas por IP PÃºblico)</option>
+                                        <option value="32">32 (~2000 portas por IP PÃºblico)</option>
+                                        <option value="64" selected="">64 (~1000 portas por IP PÃºblico)</option>
+                                        <option value="128">128 (~500 portas por IP PÃºblico) NÃ£o recomendado</option>
+                                        </select>
+					<small id="obs" class="form-text text-muted">NÂº de clientes para cada X nÂº de portas sobre 1 Ãºnico IP pÃºblico </small>
+                                </div>
+				<div class="form-group col-md-4">
+					<label for="intpub"><b>Nome da interface para adicionar os IPs PÃºblicos</b></label>
 					<input type="text" class="form-control" id="intpub" name="intpub" placeholder="ex: sfp-sfpplus1">
-					<small id="obs" class="form-text text-muted">O bloco público será quebrado em /32 e adicionar a uma interface, informe o nome da mesma.</small>
+					<small id="obs" class="form-text text-muted">Bloco pÃºblico serÃ¡ quebrado em /32 e adicionar a uma interface.</small>
 				</div>
+				<div class="form-group col-md-4">
+                                        <label for="x"><b>Sequencial da Chain</b></label>
+                                        <input type="text" class="form-control" id="x" name="x" placeholder="0" value='0'>
+					<small id="obs" class="form-text text-muted">CGNAT-0, CGNAT-1, ...</small>
+                                </div>
 			</div>
 			<div class="form-row">
 				<div class="form-group col-md-6">
 					<label for="int"><b>Interface Uplink</b></label>
 					<select class="custom-select" name="int" id="int" onChange="dis_able()">
-						<option value="null">Não Informar</option>
+						<option value="null">NÃ£o Informar</option>
 						<option value="nome">Nome da Interface</option>
 						<option value="list">Interface List</option>				
 					</select>
-					<small id="obs" class="form-text text-muted">Se optar por int. list não esqueça de adicionar sua uplink a ela.</small>
+					<small id="obs" class="form-text text-muted">Se optar por int. list nÃ£o esqueÃ§a de adicionar sua uplink a ela.</small>
 				</div>	
 				<div class="form-group col-md-6">
 					<label for="nome"><b>Nome da interface</b></label>
@@ -80,7 +90,7 @@
 						<input type="radio" class="form-check-input" name="protocol" value="tcpudp">Apenas TCP
 					</label>
 				</div>
-				<small id="obs" class="form-text text-muted">Algumas pessoas já alegaram ter algum problema fazendo para UDP.</small>
+				<small id="obs" class="form-text text-muted">Algumas pessoas jÃ¡ alegaram ter algum problema fazendo para UDP.</small>
 			</div>
 			<hr>
 			<div class="form-group">
@@ -96,14 +106,14 @@
 			$get_ip_mask = explode("/",$_POST['s']);
 
 			if(!filter_var($_POST['c'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-			  echo "IP inicial inválido.". $_POST['c']; die;
+			  echo "IP inicial invÃ¡lido.". $_POST['c']; die;
 			}
 
 			if(!filter_var($get_ip_mask[0], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-			  echo "Bloco público inválido.".$get_ip_mask[0]; die;
+			  echo "Bloco pÃºblico invÃ¡lido.".$get_ip_mask[0]; die;
 			}
 			if($get_ip_mask[1] == '31') { 
-				echo "Máscara não suportada."; die;
+				echo "MÃ¡scara nÃ£o suportada."; die;
 			}
 
 			$ip_count = 1 << (32 - $get_ip_mask[1]);
@@ -134,7 +144,7 @@
 				echo "<br /># Cria interface list <br />";
 				echo "/interface list add name=" .$_POST['nome']. "<br /><br />";
 
-				echo "# Não esqueça de adicionar sua interface de Uplink ao grupo ".$_POST['nome']." <br />";
+				echo "# NÃ£o esqueÃ§a de adicionar sua interface de Uplink ao grupo ".$_POST['nome']." <br />";
 				echo "/interface list member add list=".$_POST['nome']." interface=\"<b>*****</b>\" <br /><br />";
 
 				$interface_saida = "out-interface-list";
@@ -163,13 +173,14 @@
 			$CGNAT_RULES = $_POST['t'];
 			$saida_regras = array();
 			$saida_jumps = array();
-			$x = 1;
+			$x = $_POST['x'];
 
 			$rules = explode('/', $CGNAT_START);
 			$ports = ceil((65535-1024)/$CGNAT_RULES);
 			$ports_start = 1025;
 			$ports_end = $ports_start + $ports;
 
+			$public = explode('.', $rules[0]);
 			$CGNAT_IP_INICIAL = $CGNAT_IP;
 			$checkip = $CGNAT_IP_INICIAL;
 
@@ -217,7 +228,7 @@
 		}
 	?>
 	<div align="center">
-		<a href="https://blog.remontti.com.br/doar">Fazer uma Doação</a> | <a href="https://github.com/remontti/php-cgnat-netmap-routeros">Código Fonte</a>
+		<a href="https://blog.remontti.com.br/doar">Fazer uma DoaÃ§Ã£o</a> | <a href="https://github.com/remontti/php-cgnat-netmap-routeros">CÃ³digo Fonte</a>
 		<p><small>Desenvolvido por Rudimar Remontti</small></p>
 		<div id="fb-root"></div>
 		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v6.0&appId=630735496970056&autoLogAppEvents=1"></script>
